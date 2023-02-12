65e90463afed06e57a832f111f443424c363f81e
Juan Pablo Sánchez Rey <juanpablosanchezrey22@gmail.com>
2023-02-12 13:35:42 -0500

    view country complete

--- src/app/country/pages/view-country/view-country.component.ts
+++ src/app/country/pages/view-country/view-country.component.ts
@@ -1,22 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs';
+
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-view-country',
   templateUrl: './view-country.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ViewCountryComponent implements OnInit {
+  country!: Country;
 
-  constructor( private activatedRoute: ActivatedRoute ) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countryService: CountryService
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.countryService.getCountryById(id)),
+      tap( console.log )
+      )
+      .subscribe(country => this.country = country[0]);
 
-    this.activatedRoute.params.subscribe (params => {
-      console.log(params);
-    })
-
+    // this.activatedRoute.params.subscribe(({ id }) => {
+    //   console.log(id);
+    //   this.countryService.getCountryById(id).subscribe((country) => {
+    //     console.log(country);
+    //   });
+    // });
   }
-
 }
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -13,13 +13,16 @@ export class CountryService {
 
   searchCountry(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/name/${term}`;
-
     return this.http.get<Country[]>(url);
   }
 
-  searchCapital( term: string):Observable<Country[]> {
+  searchCapital(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`;
+    return this.http.get<Country[]>(url);
+  }
 
+  getCountryById(id: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/alpha/${id}`;
     return this.http.get<Country[]>(url);
   }
 }
